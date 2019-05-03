1a907bf8fec67581393127701a557d305719d3cc
Martin Buberl <hello@martinbuberl.com>
2019-05-03 13:05:14 +0300

    Fixed ESLint issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-const { parallel, src, dest } = require('gulp');
-const sass = require('gulp-sass');
-const cleancss = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
+const { parallel, src, dest } = require('gulp')
+const sass = require('gulp-sass')
+const cleancss = require('gulp-clean-css')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
 
-function scripts(cb) {
+function scripts (cb) {
   src(['content/js/base.js'])
     .pipe(concat('base.min.js'))
     .pipe(uglify({
       compress: { drop_debugger: false }
     }))
-    .pipe(dest('content'));
+    .pipe(dest('content'))
 
-  cb();
+  cb()
 }
 
-function styles(cb) {
+function styles (cb) {
   src([
     'content/css/import.scss',
     'content/normalize.css',
     'content/css/base.scss'
-    ])
+  ])
     .pipe(sass())
     .pipe(concat('all.min.css'))
     .pipe(cleancss({
@@ -28,9 +28,9 @@ function styles(cb) {
       processImport: false,
       keepSpecialComments: false
     }))
-    .pipe(dest('content'));
+    .pipe(dest('content'))
 
-  cb();
+  cb()
 }
 
-exports.default = parallel(scripts, styles);
+exports.default = parallel(scripts, styles)

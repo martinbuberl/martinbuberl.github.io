29b6df5af18f85a19dd5345866b7d99ab4bb0ea0
Martin Buberl <hello@martinbuberl.com>
2019-05-03 13:10:39 +0300

    Fixed ESLint issues

--- content/js/base.js
+++ content/js/base.js
@@ -1,42 +1,43 @@
-(function (w, d) {
+(function (w, d, $) {
   $(d).on('keydown', function (e) {
     switch (e.keyCode) {
       case 37:
-        var $prev = $("article a.prev");
-        var $prevHidden = $("article a.prev.hidden");
+        var $prev = $('article a.prev')
+        var $prevHidden = $('article a.prev.hidden')
 
         if ($prev.length && $prevHidden.length === 0) {
-          w.location.href = $prev.attr('href');
+          w.location.href = $prev.attr('href')
         }
-        break;
+        break
       case 39:
-        var $next = $("article a.next");
-        var $nextHidden = $("article a.next.hidden");
+        var $next = $('article a.next')
+        var $nextHidden = $('article a.next.hidden')
 
         if ($next.length && $nextHidden.length === 0) {
-          w.location.href = $next.attr('href');
+          w.location.href = $next.attr('href')
         }
-        break;
+        break
     }
-  });
+  })
 
   $('.share').on('click', function (e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    share.apply(this, ['twitter', 550, 250]);
-    share.apply(this, ['facebook', 550, 300]);
-    share.apply(this, ['google', 500, 550]);
-    share.apply(this, ['hackernews', 500, 250]);
-  });
+    share.apply(this, ['twitter', 550, 250])
+    share.apply(this, ['facebook', 550, 300])
+    share.apply(this, ['google', 500, 550])
+    share.apply(this, ['hackernews', 500, 250])
+  })
   var share = function (network, width, height) {
-    var $this = $(this);
+    var $this = $(this)
 
     if ($this.hasClass(network)) {
       if (typeof w.ga !== 'undefined') {
-        w.ga('send', 'event', 'share', network);
+        w.ga('send', 'event', 'share', network)
       }
 
-      w.open($this.attr('href'), 'share-' + network, 'width=' + width + ',height=' + height);
+      w.open($this.attr('href'), 'share-' + network, 'width=' + width + ',height=' + height)
     }
-  };
-})(window, document);
+  }
+// eslint-disable-next-line no-undef
+})(window, document, jQuery)
